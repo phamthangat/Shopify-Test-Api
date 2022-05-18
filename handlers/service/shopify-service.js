54223953ca6735209643a88b71d026222d4cc44d
thangpham <phamthangat397@gmail.com>
2022-05-18 10:50:23 +0700

    fix token

--- handlers/service/shopify-service.js
+++ handlers/service/shopify-service.js
@@ -1,11 +1,13 @@
 
 const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const instance = axios.create({
     baseURL: "https://shopth123.myshopify.com/admin/api/2022-01/",
     timeout: 1000,
     headers: {
-      "X-Shopify-Access-Token": "",
+      "X-Shopify-Access-Token": `${process.env.App_Store_Token}`,
       "content-type": "application/json",
     },
   });
